f891b18ba69b43486a94724b10583af348a1c377
rakib-utsho <rakibutsho1920@gmail.com>
2026-07-07 10:11:38 +0600

    fix: remove PersistGate to resolve fatal hydration mismatch causing blank screen on Vercel

--- src/redux/Provider.tsx
+++ src/redux/Provider.tsx
@@ -3,14 +3,12 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { persistor, store } from "./store";
-import { PersistGate } from "redux-persist/integration/react";
+
 
 const ReduxProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {children}
-      </PersistGate>
+      {children}
     </Provider>
   );
 };
